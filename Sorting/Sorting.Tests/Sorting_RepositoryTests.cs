39da1dc43eb07e17c17628d4fe87a1e4954778d0
Iurii-Antoniuk <iurii.antoniuk@gmail.com>
2020-08-02 18:22:04 +0200

    init commit

---
+++ Sorting/Sorting.Tests/Sorting_RepositoryTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using NUnit.Framework;
+using Sorting;
+
+namespace Sorting.Tests
+{
+    [TestFixture]
+    public class Sorting_RepositoryTests
+    {
+        private int[][] Samples()
+        {
+            int[][] samples = new int[9][];
+            samples[0] = new[] { 1 };
+            samples[1] = new[] { 2, 1 };
+            samples[2] = new[] { 2, 1, 3 };
+            samples[3] = new[] { 1, 1, 1 };
+            samples[4] = new[] { 2, -1, 3, 3 };
+            samples[5] = new[] { 4, -5, 3, 3 };
+            samples[6] = new[] { 0, -5, 3, 3 };
+            samples[7] = new[] { 0, -5, 3, 0 };
+            samples[8] = new[] { 3, 2, 5, 5, 1, 0, 7, 8 };
+
+            return samples;
+        }
+
+        private void PrintOut(int[] array)
+        {
+            TestContext.WriteLine("-----TRACE-----\n");
+            foreach (var el in array)
+            {
+                TestContext.Write(el + " ");
+            }
+            TestContext.WriteLine("\n----------------------\n");
+        }
+
+        private void RunTestsForSortAlgorithms(Action<int[]> sort)
+        {
+            foreach (var sample in Samples())
+            {
+                sort(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintOut(sample);
+            }
+        }
+
+        [Test]
+        public void BubbleSort_ValidInput_SortedInput()
+        {
+            RunTestsForSortAlgorithms(Sorting_Repository.BubbleSort);
+        }
+
+        [Test]
+        public void SelectionSort_ValidInput_SortedInput()
+        {
+            RunTestsForSortAlgorithms(Sorting_Repository.SelectionSort);
+        }
+
+        [Test]
+        public void InsertionSort_ValidInput_SortedInput()
+        {
+            RunTestsForSortAlgorithms(Sorting_Repository.InsertionSort);
+        }
+
+        [Test]
+        public void MergeSort_ValidInput_SortedInput()
+        {
+            RunTestsForSortAlgorithms(Sorting_Repository.MergeSort);
+        }
+
+        [Test]
+        public void QuickSort_ValidInput_SortedInput()
+        {
+            RunTestsForSortAlgorithms(Sorting_Repository.QuickSort);
+        }
+    }
+}
---
+++ Sorting/Sorting/Sorting_Repository.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Globalization;
+
+namespace Sorting
+{
+    public class Sorting_Repository
+    {
+        private static void Swap(int[]array, int i, int j)
+        {
+            if (i == j)
+                return;
+            else
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int partInd = array.Length-1; partInd > 0; partInd--)
+            {
+                for (int i = 0; i < partInd; i++)
+                {
+                    if (array[i] > array[i + 1])
+                        Swap(array, i, i + 1);
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int partInd = array.Length - 1; partInd > 0; partInd--)
+            {
+                int largestInd = 0;
+                for (int i = 1; i <= partInd; i++)
+                {
+                    if (array[i] > array[largestInd])
+                        largestInd = i;
+                }
+                Swap(array, largestInd, partInd);
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (int partInd = 1; partInd < array.Length; partInd++)
+            {
+                int currentUnsorted = array[partInd];
+                int i;
+                for (i = partInd; (i > 0) && (array[i-1] > currentUnsorted); i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[i] = currentUnsorted;
+            }
+        }
+
+        public static void MergeSort(int[] array)
+        {
+            int[] aux = new int[array.Length];
+            Sort(0, array.Length-1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                    return;
+
+                int mid = (high + low) / 2;
+                Sort(low, mid);
+                Sort(mid+1, high);
+                Array.Copy(array, low, aux, low, high - low + 1);
+                Merge(low, mid, high);
+            }
+
+            void Merge(int low, int mid, int high)
+            {
+                if (array[mid] <= array[mid + 1])
+                    return;
+                
+                int i = low;
+                int j = mid+1;
+
+                for (int k = low; k <= high; k++)
+                {
+                    if (i > mid) array[k] = aux[j++];
+                    else if (j > high) array[k] = aux[i++];
+                    else if (aux[j] < aux[i]) 
+                        array[k] = aux[j++];
+                    else
+                        array[k] = aux[i++];
+                }
+            }
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            Sort(0, array.Length-1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                    return;
+
+                int p = Partition(low, high);
+                Sort(low, p - 1);
+                Sort(p+1, high);
+            }
+
+            int Partition(int low, int high)
+            {
+                int i = low;
+                int j = high + 1;
+                int pivot = array[low];
+
+                while(true)
+                {
+                    while(array[++i] < pivot)
+                    {
+                        if (i >= high)
+                            break;
+                    }
+                    while(array[--j] > pivot)
+                    {
+                        if (i <= low)
+                            break;
+                    }
+
+                    if (j <= i)
+                        break;
+                    Swap(array, i, j);
+                }
+                Swap(array, low, j);
+                return j;
+            }
+        }
+    }
+}
